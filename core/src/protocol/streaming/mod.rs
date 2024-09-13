03b46299e51a378f0c4043ee511ca23187e0df52
Aaro Altonen <a.altonen@hotmail.com>
2024-09-13 19:27:49 +0300

    streaming: implement `Stream::new_outbound()`

--- core/src/protocol/streaming/mod.rs
+++ core/src/protocol/streaming/mod.rs
@@ -15,3 +15,51 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
+
+use crate::runtime::Runtime;
+
+use bytes::{BufMut, BytesMut};
+use rand_core::RngCore;
+
+use core::marker::PhantomData;
+
+pub struct Stream<R: Runtime> {
+    recv_stream_id: u32,
+    send_stream_id: Option<u32>,
+    seq_nro: u32,
+
+    /// Marker for `Runtime`.
+    _runtime: PhantomData<R>,
+}
+
+impl<R: Runtime> Stream<R> {
+    pub fn new_outbound() -> (Self, BytesMut) {
+        let mut payload = "GET / HTTP/1.1\r\n\n".as_bytes();
+        let mut out = BytesMut::with_capacity(payload.len() + 22);
+
+        let recv_stream_id = R::rng().next_u32();
+        let seq_nro = 0u32;
+
+        out.put_u32(0u32); // send stream id
+        out.put_u32(recv_stream_id);
+        out.put_u32(seq_nro);
+        out.put_u32(0u32); // ack through
+        out.put_u8(0u8); // nack count
+
+        // TODO: signature
+        out.put_u8(10u8); // resend delay, in seconds
+        out.put_u16(1u16); // flags: syn
+        out.put_u16(0u16); // options
+        out.put_slice(&payload);
+
+        (
+            Self {
+                recv_stream_id,
+                send_stream_id: None,
+                seq_nro,
+                _runtime: Default::default(),
+            },
+            out,
+        )
+    }
+}
