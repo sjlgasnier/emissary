b07ecc0715a745a6c0d3e450fd87828f3fc0a0ee
Aaro Altonen <a.altonen@hotmail.com>
2024-10-03 14:44:47 +0300

    i2cp: implement `I2cpServer`
    `I2cpServer`'s responsiblity is to poll the tcp socket for inbound
    connections, read the i2cp control byte from the socket and if the byte
    is a valid control byte, spawn a new future for the i2cp session

--- core/src/i2cp/mod.rs
+++ core/src/i2cp/mod.rs
@@ -21,9 +21,14 @@
 //! https://geti2p.net/en/docs/protocol/i2cp
 
 use crate::{
-    runtime::{Runtime, TcpListener},
+    error::I2cpError,
+    i2cp::session::I2cpSession,
+    runtime::{JoinSet, Runtime, TcpListener},
+    util::AsyncReadExt,
     Error,
 };
+
+use futures::StreamExt;
 
 use core::{
     future::Future,
@@ -32,13 +37,24 @@ use core::{
     task::{Context, Poll},
 };
 
+mod session;
+
 /// Logging target for the file.
 const LOG_TARGET: &str = "emissary::i2cp";
 
+/// I2CP protocol byte.
+const I2CP_PROTOCOL_BYTE: u8 = 0x2a;
+
 /// I2CP server
+///
+/// Listens to incoming I2CP streams and dispatches them to a separate event loop
+/// after the I2CP protocol byte has been received.
 pub struct I2cpServer<R: Runtime> {
     /// TCP listener.
     listener: R::TcpListener,
+
+    /// Pending connections.
+    pending_connections: R::JoinSet<crate::Result<R::TcpStream>>,
 }
 
 impl<R: Runtime> I2cpServer<R> {
@@ -55,14 +71,80 @@ impl<R: Runtime> I2cpServer<R> {
             .await
             .ok_or(Error::IoError(String::from("failed to bind i2cp socket")))?;
 
-        Ok(Self { listener })
+        Ok(Self {
+            listener,
+            pending_connections: R::join_set(),
+        })
     }
 }
 
 impl<R: Runtime> Future for I2cpServer<R> {
     type Output = ();
 
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        loop {
+            match self.listener.poll_accept(cx) {
+                Poll::Pending => break,
+                Poll::Ready(None) => {
+                    tracing::error!(
+                        target: LOG_TARGET,
+                        "ready `None` from i2cp server socket",
+                    );
+
+                    return Poll::Ready(());
+                }
+                Poll::Ready(Some(mut stream)) => {
+                    tracing::debug!(
+                        target: LOG_TARGET,
+                        "incoming connection, read protocol byte",
+                    );
+
+                    // complete handshake for the i2cp client session in the background by polling
+                    // the connection until the protocol byte is received and comparing it against
+                    // the expected protocol byte
+                    self.pending_connections.push(async move {
+                        let mut protocol_byte = vec![0u8; 1];
+
+                        stream.read_exact(&mut protocol_byte).await?;
+
+                        if protocol_byte[0] != I2CP_PROTOCOL_BYTE {
+                            return Err(Error::I2cp(I2cpError::InvalidProtocolByte(
+                                protocol_byte[0],
+                            )));
+                        }
+
+                        Ok(stream)
+                    });
+                }
+            }
+        }
+
+        loop {
+            match self.pending_connections.poll_next_unpin(cx) {
+                Poll::Pending => break,
+                Poll::Ready(None) => {
+                    tracing::error!(
+                        target: LOG_TARGET,
+                        "read `None` from pending connections",
+                    );
+                    return Poll::Ready(());
+                }
+                Poll::Ready(Some(Err(error))) => tracing::warn!(
+                    target: LOG_TARGET,
+                    ?error,
+                    "failed to accept inbound i2cp connection",
+                ),
+                Poll::Ready(Some(Ok(stream))) => {
+                    tracing::debug!(
+                        target: LOG_TARGET,
+                        "i2cp client session accepted",
+                    );
+
+                    R::spawn(I2cpSession::<R>::new(stream));
+                }
+            }
+        }
+
         Poll::Pending
     }
 }
