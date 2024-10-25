bbd4e072a944b76631b10d3fb5d8e6a7fb8dd67b
Aaro Altonen <a.altonen@hotmail.com>
2024-10-25 21:41:26 +0300

    sam: handle `SESSION CREATE` in `PendingSamConnection`

--- core/src/sam/pending.rs
+++ core/src/sam/pending.rs
@@ -30,7 +30,9 @@ use hashbrown::HashMap;
 
 use alloc::{boxed::Box, format, string::String};
 use core::{
+    fmt,
     future::Future,
+    mem,
     pin::Pin,
     task::{Context, Poll},
     time::Duration,
@@ -108,6 +110,35 @@ pub enum ConnectionKind<R: Runtime> {
     },
 }
 
+impl<R: Runtime> fmt::Debug for ConnectionKind<R> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Session {
+                version,
+                session_id,
+                session_kind,
+                options,
+                ..
+            } => f
+                .debug_struct("ConnectionKind::Session")
+                .field("version", &version)
+                .field("session_id", &session_id)
+                .field("session_kind", &session_kind)
+                .field("options", &options)
+                .finish_non_exhaustive(),
+            Self::Stream {
+                version,
+                destination,
+                options,
+                ..
+            } => f.debug_struct("ConnectionKind::Stream").finish_non_exhaustive(),
+            Self::Accept { .. } => f.debug_struct("ConnectionKind::Accept").finish_non_exhaustive(),
+            Self::Forward { .. } =>
+                f.debug_struct("ConnectionKind::Forward").finish_non_exhaustive(),
+        }
+    }
+}
+
 /// Connection state.
 ///
 /// Connection starts by the client and server agreeing on a SAMv3 version after which the client
@@ -121,16 +152,24 @@ pub enum ConnectionKind<R: Runtime> {
 /// SAMv3 command and leaves the validation of the command with respect to the overall connection
 /// state to `SamServer` which ensures that for stream-related commands, there exists an active
 /// session.
-#[derive(Debug)]
-enum PendingConnectionState {
+enum PendingConnectionState<R: Runtime> {
     /// Awaiting handshake from client.
-    AwaitingHandshake,
+    AwaitingHandshake {
+        /// Socket used to read SAMv3 commands from client.
+        socket: SamSocket<R>,
+    },
 
     /// Session has been handshaked.
     Handshaked {
+        /// Socket used to read SAMv3 commands from client.
+        socket: SamSocket<R>,
+
         /// Negotiated SAMv3 version.
         version: SamVersion,
     },
+
+    /// Connection state has been poisoned.
+    Poisoned,
 }
 
 /// Pending SAMv3 connection.
@@ -143,11 +182,8 @@ enum PendingConnectionState {
 ///
 /// The last three kinds require there to be an active session.
 pub struct PendingSamConnection<R: Runtime> {
-    /// Socket used to read SAMv3 commands from client.
-    socket: SamSocket<R>,
-
     /// Connection state.
-    state: PendingConnectionState,
+    state: PendingConnectionState<R>,
 
     /// Keep-alive timer.
     keep_alive_timer: BoxFuture<'static, ()>,
@@ -157,8 +193,9 @@ impl<R: Runtime> PendingSamConnection<R> {
     /// Create new [`PendingSamConnection`].
     pub fn new(stream: R::TcpStream) -> Self {
         Self {
-            socket: SamSocket::new(stream),
-            state: PendingConnectionState::AwaitingHandshake,
+            state: PendingConnectionState::AwaitingHandshake {
+                socket: SamSocket::new(stream),
+            },
             keep_alive_timer: Box::pin(R::delay(KEEP_ALIVE_TIMEOUT)),
         }
     }
@@ -169,46 +206,102 @@ impl<R: Runtime> Future for PendingSamConnection<R> {
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         loop {
-            match (self.socket.poll_next_unpin(cx), &self.state) {
-                (Poll::Pending, _) => break,
-                (Poll::Ready(None), _) => {
-                    tracing::debug!(
-                        target: LOG_TARGET,
-                        "client closed socket",
-                    );
-                    return Poll::Ready(Err(Error::Connection(ConnectionError::SocketClosed)));
-                }
-                (
-                    Poll::Ready(Some(SamCommand::Hello { min, max })),
-                    &PendingConnectionState::AwaitingHandshake,
-                ) => {
-                    // default to client's maximum supported version and if they didn't provide
-                    // a version, default to server's maximum supported version which is SAMv3.3
-                    let version = max.unwrap_or(SamVersion::V33);
+            match mem::replace(&mut self.state, PendingConnectionState::Poisoned) {
+                PendingConnectionState::AwaitingHandshake { mut socket } => match socket
+                    .poll_next_unpin(cx)
+                {
+                    Poll::Pending => {
+                        self.state = PendingConnectionState::AwaitingHandshake { socket };
+                        break;
+                    }
+                    Poll::Ready(None) => {
+                        tracing::debug!(
+                            target: LOG_TARGET,
+                            "client closed socket",
+                        );
+                        return Poll::Ready(Err(Error::Connection(ConnectionError::SocketClosed)));
+                    }
+                    Poll::Ready(Some(SamCommand::Hello { min, max })) => {
+                        // default to client's maximum supported version and if they didn't provide
+                        // a version, default to server's maximum supported version which is SAMv3.3
+                        let version = max.unwrap_or(SamVersion::V33);
 
-                    tracing::debug!(
-                        target: LOG_TARGET,
-                        ?version,
-                        "client connected"
-                    );
+                        tracing::debug!(
+                            target: LOG_TARGET,
+                            ?version,
+                            "client connected"
+                        );
 
-                    self.socket.send_message(
-                        format!("HELLO REPLY RESULT=OK VERSION={version}\n").as_bytes().to_vec(),
-                    );
-                    self.state = PendingConnectionState::Handshaked { version };
+                        socket.send_message(
+                            format!("HELLO REPLY RESULT=OK VERSION={version}\n")
+                                .as_bytes()
+                                .to_vec(),
+                        );
+                        self.state = PendingConnectionState::Handshaked { version, socket };
 
-                    // reset keep-alive timeout so the client has another 10 seconds to send the
-                    // next command before the connection is closed
-                    self.keep_alive_timer = Box::pin(R::delay(KEEP_ALIVE_TIMEOUT));
-                }
-                (Poll::Ready(Some(SamCommand::Hello { min, max })), state) => {
+                        // reset keep-alive timeout so the client has another 10 seconds to send the
+                        // next command before the connection is closed
+                        self.keep_alive_timer = Box::pin(R::delay(KEEP_ALIVE_TIMEOUT));
+                    }
+                    Poll::Ready(Some(command)) => {
+                        tracing::debug!(
+                            target: LOG_TARGET,
+                            ?command,
+                            "received an unexpected command, expected `HELLO`",
+                        );
+                        return Poll::Ready(Err(Error::InvalidState));
+                    }
+                },
+                PendingConnectionState::Handshaked {
+                    mut socket,
+                    version,
+                } => match socket.poll_next_unpin(cx) {
+                    Poll::Pending => {
+                        self.state = PendingConnectionState::Handshaked { socket, version };
+                        break;
+                    }
+                    Poll::Ready(None) => {
+                        tracing::debug!(
+                            target: LOG_TARGET,
+                            "client closed socket",
+                        );
+                        return Poll::Ready(Err(Error::Connection(ConnectionError::SocketClosed)));
+                    }
+                    Poll::Ready(Some(SamCommand::CreateSession {
+                        session_id,
+                        session_kind,
+                        options,
+                    })) => {
+                        tracing::info!(
+                            target: LOG_TARGET,
+                            ?session_id,
+                            ?session_kind,
+                            "create session"
+                        );
+
+                        return Poll::Ready(Ok(ConnectionKind::Session {
+                            stream: socket.into_inner(),
+                            version,
+                            session_id,
+                            session_kind,
+                            options,
+                        }));
+                    }
+                    Poll::Ready(Some(command)) => {
+                        tracing::debug!(
+                            target: LOG_TARGET,
+                            ?command,
+                            "received an unexpected command, expected `SESSION`/`STREAM`",
+                        );
+                        return Poll::Ready(Err(Error::InvalidState));
+                    }
+                },
+                PendingConnectionState::Poisoned => {
                     tracing::warn!(
                         target: LOG_TARGET,
-                        ?min,
-                        ?max,
-                        ?state,
-                        "invalid state to receive `HELLO`",
+                        "pending connection state has been poisoned",
                     );
+                    debug_assert!(false);
                     return Poll::Ready(Err(Error::InvalidState));
                 }
             }
@@ -285,6 +378,7 @@ mod tests {
             match connection.state {
                 PendingConnectionState::Handshaked {
                     version: SamVersion::V33,
+                    ..
                 } => break,
                 _ => {}
             }
@@ -329,6 +423,7 @@ mod tests {
             match connection.state {
                 PendingConnectionState::Handshaked {
                     version: SamVersion::V33,
+                    ..
                 } => break,
                 _ => {}
             }
@@ -367,6 +462,7 @@ mod tests {
             match connection.state {
                 PendingConnectionState::Handshaked {
                     version: SamVersion::V32,
+                    ..
                 } => break,
                 _ => {}
             }
@@ -405,6 +501,7 @@ mod tests {
             match connection.state {
                 PendingConnectionState::Handshaked {
                     version: SamVersion::V33,
+                    ..
                 } => break,
                 _ => {}
             }
@@ -418,5 +515,86 @@ mod tests {
         reader.read_line(&mut response).await.unwrap();
 
         assert_eq!(response, "HELLO REPLY RESULT=OK VERSION=3.3\n");
+    }
+
+    #[tokio::test]
+    async fn session_create() {
+        let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+        let address = listener.local_addr().unwrap();
+        let (stream1, stream2) = tokio::join!(listener.accept(), MockTcpStream::connect(address));
+
+        let mut connection = PendingSamConnection::<MockRuntime>::new(stream2.unwrap());
+        let mut stream = stream1.unwrap().0;
+
+        // send handshake
+        stream.write_all(b"HELLO VERSION\n").await.unwrap();
+
+        // poll pending connection until it's handshaked
+        loop {
+            futures::future::poll_fn(|cx| match connection.poll_unpin(cx) {
+                Poll::Pending => Poll::Ready(()),
+                _ => panic!("invalid return value"),
+            })
+            .await;
+
+            match connection.state {
+                PendingConnectionState::Handshaked {
+                    version: SamVersion::V33,
+                    ..
+                } => break,
+                _ => {}
+            }
+
+            tokio::time::sleep(Duration::from_secs(1)).await;
+        }
+
+        // read and validate handshake response
+        let mut reader = BufReader::new(stream);
+        let mut response = String::new();
+        reader.read_line(&mut response).await.unwrap();
+
+        assert_eq!(response, "HELLO REPLY RESULT=OK VERSION=3.3\n");
+
+        // send handshake
+        let mut stream = reader.into_inner();
+        stream
+            .write_all(b"SESSION CREATE STYLE=STREAM ID=test DESTINATION=TRANSIENT\n")
+            .await
+            .unwrap();
+
+        match tokio::time::timeout(Duration::from_secs(5), connection).await.unwrap() {
+            Ok(ConnectionKind::Session {
+                session_id,
+                version: SamVersion::V33,
+                session_kind: SessionKind::Stream,
+                options,
+                ..
+            }) => {
+                assert_eq!(session_id.as_str(), "test");
+            }
+            Ok(kind) => panic!("invalid connection kind: {kind:?}"),
+            Err(error) => panic!("failed to create session: {error:?}"),
+        }
+    }
+
+    #[tokio::test]
+    async fn send_sesssion_create_before_handshake() {
+        let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+        let address = listener.local_addr().unwrap();
+        let (stream1, stream2) = tokio::join!(listener.accept(), MockTcpStream::connect(address));
+
+        let mut connection = PendingSamConnection::<MockRuntime>::new(stream2.unwrap());
+        let mut stream = stream1.unwrap().0;
+
+        stream
+            .write_all(b"SESSION CREATE STYLE=STREAM ID=test DESTINATION=TRANSIENT\n")
+            .await
+            .unwrap();
+
+        match tokio::time::timeout(Duration::from_secs(5), connection).await.unwrap() {
+            Err(Error::InvalidState) => {}
+            Ok(kind) => panic!("session succeeded: {kind:?}"),
+            Err(error) => panic!("invalid error: {error:?}"),
+        }
     }
 }
